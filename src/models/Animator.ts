118d23486e1394bba283800ff4e6fe2e9c810093
Jawoon Kim <jawoon.kim@edu.bib.de>
2021-11-16 17:09:25 +0100

    installed animejs

--- src/models/Animator.ts
+++ src/models/Animator.ts
@@ -1,48 +1,9 @@
-import { Animations } from "./Animations";
+import anime from "animejs";
 
 export abstract class Animator {
-  public static animate(animation: Animations) {
-    switch (animation) {
-      case Animations.OpeningSequence:
-        break;
-      case Animations.EndingSequence:
-        break;
-      case Animations.RocketSmoke:
-        break;
-      case Animations.RocketFlame:
-        break;
-      case Animations.RocketFloat:
-        break;
-      case Animations.RocketStart:
-        break;
-      case Animations.RocketShake:
-        break;
-      case Animations.QuizClear:
-        break;
-      case Animations.QuizNext:
-        break;
-      case Animations.QuizDrop:
-        break;
-      case Animations.CountCurrentScore:
-        break;
-      case Animations.CountPoints:
-        break;
-      case Animations.CountGauge:
-        break;
-      case Animations.CountSpeed:
-        break;
-      case Animations.CountCombo:
-        break;
-      case Animations.CountLevel:
-        break;
-      case Animations.CountScoreboard:
-        break;
-      case Animations.CorrectAnswer:
-        break;
-      case Animations.WrongAnswer:
-        break;
-      default:
-        break;
-    }
+  public static rocketShake() {
+    anime({
+
+    });
   }
 }
